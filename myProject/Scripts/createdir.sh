8a9c5cc327a08ddb00fcb4c5dd165d27b19950f3
manishaverma89 <84954924+manishaverma89@users.noreply.github.com>
2023-12-29 11:40:41 +0530

    Update createdir.sh

--- myProject/Scripts/createdir.sh
+++ myProject/Scripts/createdir.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/Inventory/hostfile /home/ANSIBLE/myFirstAnsibleProject/myProject/Ansible/callingroles/callcreatedir.yaml
+ansible-playbook -i /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/Inventory/hostfile /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/ansible/callingroles/callcreatedir.yaml
