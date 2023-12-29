5d94f3674ca04bb360fc701380d41347f4e3a1ba
manishaverma89 <manisha.verma23@gmail.com>
2023-12-29 11:34:57 +0530

    Added Role createdir

--- myProject/Scripts/myscript.sh
+++ myProject/Scripts/myscript.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 ansible-playbook -i /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/Inventory/hostfile /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/Ansible/callingmyrole.yaml
+
