a0665a5cab373a1927424d3e6f8bdb63ecb52e2f
manishaverma89 <manisha.verma23@gmail.com>
2023-12-29 11:20:15 +0530

    create directory on centos nodes

---
+++ myProject/Scripts/createdir.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/Inventory/hostfile /home/ANSIBLE/myFirstAnsibleProject/myProject/Ansible/callingroles/callcreatedir.yaml
