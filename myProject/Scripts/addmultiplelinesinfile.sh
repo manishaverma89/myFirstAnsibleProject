a462e1e84ba5c4ad792c8cc43456883409d27f8b
manishaverma89 <manisha.verma23@gmail.com>
2023-12-30 10:42:18 +0530

    Added addmultiplelinesinfile-lineinfilemodule

---
+++ myProject/Scripts/addmultiplelinesinfile.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/Inventory/hostfile  /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/ansible/calladdmultiplelinesinfile-lineinfilemodule.yaml
