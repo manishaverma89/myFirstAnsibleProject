bc476225c46039400d344c231889d1e5d3a64395
manishaverma89 <84954924+manishaverma89@users.noreply.github.com>
2024-01-01 22:57:02 +0530

    Update loops.sh

--- myProject/Scripts/loops.sh
+++ myProject/Scripts/loops.sh
@@ -1,2 +1,2 @@
-ansible-playbook  --diff -i /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/Inventory/hostfile  /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/Ansible/callloops.yaml
+ansible-playbook  -i /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/Inventory/hostfile  /home/ansible/ansible-practice/myFirstAnsibleProject/myProject/Ansible/callloops.yaml
 
